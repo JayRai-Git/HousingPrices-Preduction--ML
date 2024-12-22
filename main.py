636bed15c946a6546a97b86c267f4311c514090c
Rai <jay.rai@siemens-energy.com>
2024-12-23 00:28:06 +0530

    initilizing the Advance  Housing Prices Preduction Second_Commit

--- main.py
+++ main.py
@@ -1,141 +1,95 @@
-import numpy as np
+#Overview of Project 
+
+#import the necessary package 
+
 import pandas as pd 
+import numpy as np 
+
+#load Traning Data 
+Train_data=pd.read_csv("/kaggle/input/house-prices-advanced-regression-techniques/train.csv")
+print(Train_data.head())
+print(Train_data.columns)
+#Load Test Data
+Test_data=pd.read_csv("/kaggle/input/house-prices-advanced-regression-techniques/test.csv")
+
+print(pd.Series(Train_data.columns).value_counts().sum())
+Train_data=Train_data.drop(columns=['PoolQC','MiscFeature','Alley','Fence','GarageYrBlt','GarageCond','BsmtFinType2'],axis=1)
+Test_data=Test_data.drop(columns=['PoolQC','MiscFeature','Alley','Fence','GarageYrBlt','GarageCond','BsmtFinType2'],axis=1)
+print(Train_data.head())
+print(pd.Series(Train_data.columns).value_counts().sum())
+
+print(Train_data.dtypes[Train_data.dtypes != 'object'])
+#print(Train_data.describe())
+#print(Train_data.info())
+print(Train_data.duplicated())
+
 import matplotlib.pyplot as plt
+#print(Train_data.query('LotFrontage >200'))
+Train_data=Train_data.drop(Train_data[Train_data['LotFrontage']>200.00].index)
 
-InputFilePath=r"C:\Users\z004fpbu\Desktop\jay\MyCodes\Projects\ML_Project(HousePrice)\Inputs\Housing.csv"
-df=pd.read_csv(InputFilePath)
-print(df.shape)
-print(df.info())
+print("="*50)
+#print(Train_data.query('LotArea >100000'))
+Train_data=Train_data.drop(Train_data[Train_data['LotArea']>100000].index)
+#print(Train_data.query('OverallCond <3 & SalePrice >390000'))
+Train_data=Train_data.drop(Train_data[(Train_data['OverallCond']==2) & (Train_data['SalePrice']>390000)].index)
+#print(Train_data.query('MasVnrArea >1300 or SalePrice >700000'))
+Train_data=Train_data.drop(Train_data[(Train_data['MasVnrArea']>1300) | (Train_data['SalePrice']>700000)].index)
+Train_data=Train_data.drop(Train_data[Train_data['BedroomAbvGr']>7].index)
+Train_data=Train_data.drop(Train_data[(Train_data['KitchenAbvGr']<0.2) | (Train_data['KitchenAbvGr']>2.7)].index)
+Train_data=Train_data.drop(Train_data[Train_data['PoolArea']>450].index)
+Train_data=Train_data.drop(Train_data[Train_data['MiscVal']>8000].index)
 
-#performing EDA by using 3 party tool sweetviz (Explotery data analysis).
-#import sweetviz as sv
-#report=sv.analyze(df)
-#report.show_html("./report.html")
-
-# data preprocessing 
-#1). checking of null values
-print(df.isna().sum())
-
-#2).Drop Independent Variables(not related to analysis Output)
-
-print(df.info())
-df[['mainroad','guestroom','basement','hotwaterheating','airconditioning','prefarea']] = df[['mainroad','guestroom','basement','hotwaterheating','airconditioning','prefarea']].replace({'yes': 1, 'no': 0}).astype(int)
-
-Newdf=df
-print(Newdf)
-
-plt.scatter(x='area',y='price',data=Newdf)
-plt.xlabel('Area per sq')
-plt.ylabel('Price')
-plt.title('Scatter Plot of Price vs. Area per sq')
-plt.show()
-Newdf = Newdf.drop(Newdf[Newdf['area']>14000].index)
-print(Newdf['area'].sort_values())
-
-plt.scatter(x='parking',y='price',data=Newdf)
-plt.xlabel('Number of parking')
-plt.ylabel('Price')
-plt.title('Scatter Plot of Price vs. Number of parking')
-#plt.show()
-Newdf = Newdf.drop(Newdf[(Newdf['parking']==3.0)&(Newdf['price']>12000000)].index)
-print(Newdf['parking'].sort_values())
-
-plt.scatter(x='bedrooms',y='price',data=Newdf)
-plt.xlabel('Number of bedrooms')
-plt.ylabel('Price')
-plt.title('Scatter Plot of Price vs. Number of bedrooms')
-#plt.show()
-Newdf = Newdf.drop(Newdf[Newdf['bedrooms']==6].index)
-print(Newdf['bedrooms'].sort_values())
-
-plt.scatter(x='bathrooms',y='price',data=Newdf)
-plt.xlabel('Number of bathrooms')
-plt.ylabel('Price')
-plt.title('Scatter Plot of Price vs. Number of bathrooms')
-#plt.show()
-Newdf = Newdf.drop(Newdf[(Newdf['bedrooms']==2.00)&(Newdf['price']>12000000)].index)
-print(Newdf['bedrooms'].sort_values())
-
-plt.scatter(x='stories',y='price',data=Newdf)
-plt.xlabel('Number of stories')
-plt.ylabel('Price')
-plt.title('Scatter Plot of Price vs. Number of stories')
-#plt.show()
-Newdf = Newdf.drop(Newdf[(Newdf['stories']==3.0)&(Newdf['price']>12000000)].index)
-print(Newdf['stories'].sort_values())
-
-plt.scatter(x='mainroad',y='price',data=Newdf)
-plt.xlabel('Number of mainroad')
-plt.ylabel('Price')
-plt.title('Scatter Plot of Price vs. Number of mainroad')
-#plt.show()
-Newdf = Newdf.drop(Newdf[(Newdf['mainroad']==3.0)&(Newdf['price']>12000000)].index)
-print(Newdf['mainroad'].sort_values())
-
-print(Newdf.describe())
-plt.scatter(x='area',y='price',data=Newdf)
-plt.xlabel('area')
-plt.ylabel('Price')
-plt.title('Check data')
+plt.scatter(x='MoSold',y='SalePrice',data= Train_data)
 plt.show()
 
+print(Train_data.info())
 
-# Binding Models 
-from sklearn.preprocessing import LabelEncoder
-Newdf['furnishingstatus']=LabelEncoder().fit_transform(Newdf['furnishingstatus'])
-Newdf=Newdf.astype('int64')
-print(Newdf.info())
-x=Newdf.drop(columns=['price'])
-y=Newdf['price']
+Train_data=Train_data[Train_data["Electrical"].notna()]
+print(Train_data.info())
 
-from sklearn.model_selection import train_test_split,GridSearchCV, cross_val_score
-from sklearn.linear_model import LinearRegression,Lasso,Ridge
-from sklearn.preprocessing import StandardScaler,OneHotEncoder,OrdinalEncoder
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import make_pipeline,Pipeline
-from sklearn.metrics import mean_absolute_error,r2_score
+import seaborn as sns
 
-X_tain,X_test,Y_train,Y_test=train_test_split(x,y,train_size=0.2,random_state=42)
-print(X_tain.shape)
-print(Y_train.shape)
-print(X_test.shape)
-print(Y_test.shape)
+#print(Train_data['BsmtQual'].unique())
+Train_data['BsmtQual'].fillna('Ex',inplace=True)
+Train_data['BsmtQual'].fillna('Ex',inplace=True)
 
-#applying linear regression 
-#col_trans=make_column_transformer((OneHotEncoder(sparse_output=False),['furnishingstatus']),remainder='passthrough',n_jobs=-1)
+print(Train_data['BsmtCond'].unique())
+Train_data['BsmtCond'].fillna('TA',inplace=True)
+Train_data['BsmtCond'].fillna('TA',inplace=True)
 
-Scaler=StandardScaler()
-lr=LinearRegression()
-pipe=make_pipeline(X_tain,Scaler,lr)
-pipe.fit(X_tain,Y_train)
-y_pred_lr=pipe.predict(X_test)
-print(mean_absolute_error(Y_test,y_pred_lr))
-print(r2_score(Y_test,y_pred_lr))
+print(Train_data['BsmtExposure'].unique())
+Train_data['BsmtExposure'].fillna('Gd',inplace=True)
+Train_data['BsmtExposure'].fillna('Gd',inplace=True)
 
-#laso
-lasso=Lasso()
-pipe=make_pipeline(X_tain,Scaler,lasso)
-pipe.fit(X_tain,Y_train)
-Pipeline(steps=[( 'StandardScaling',StandardScaler()),('Lasso',Lasso())])
-y_pred_lasso=pipe.predict(X_test)
-print(r2_score(Y_test,y_pred_lasso))                
+print(Train_data['BsmtFinType1'].unique())
+Train_data['BsmtFinType1'].fillna('Unf',inplace=True)
+Train_data['BsmtFinType1'].fillna('Unf',inplace=True)
 
+print(Train_data['GarageType'].unique())
+Train_data['GarageType'].fillna('No',inplace=True)
+Train_data['GarageType'].fillna('No',inplace=True)
 
+print(Train_data['GarageFinish'].unique())
+Train_data['GarageFinish'].fillna('Unf',inplace=True)
+Train_data['GarageFinish'].fillna('Unf',inplace=True)
 
+print(Train_data['GarageQual'].unique())
+Train_data['GarageQual'].fillna('No',inplace=True)
+Train_data['GarageQual'].fillna('No',inplace=True)
 
+print("="*50)
 
+#print(Train_data['FireplaceQu'].unique())
+Train_data['FireplaceQu'].fillna('No',inplace=True)
+Train_data['FireplaceQu'].fillna('No',inplace=True)
+#sns.catplot(data=Train_data, x='FireplaceQu', y='SalePrice', kind='box')
 
+#print(Train_data['MasVnrType'].unique())
+Train_data['MasVnrType'].fillna('No',inplace=True)
+Train_data['MasVnrType'].fillna('No',inplace=True)
+#sns.catplot(data=Train_data, x='MasVnrType', y='SalePrice', kind='box')
 
-print("end")
-
-
-
-
-
-
-
-
-
-
-
-
+print(Train_data['GarageQual'].unique())
+Train_data['GarageQual'].fillna('No',inplace=True)
+Train_data['GarageQual'].fillna('No',inplace=True)
+sns.catplot(data=Train_data, x='GarageQual', y='SalePrice', kind='box')
